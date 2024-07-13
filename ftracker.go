efec321cc59e29e43ced639a42b82ad02bfa616f
Artem <artemkruchinov@yandex.ru>
2024-07-13 17:24:20 +0300

    five commit

--- ftracker.go
+++ ftracker.go
@@ -53,7 +53,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 	case trainingType == "Плавание":
 		distance := float64(lengthPool) * float64(countPool) / mInKm
 		speed := meanSpeed(action, duration)
-		calories := SwimmingSpentCalories(action, weight, duration)
+		calories := SwimmingSpentCalories(lengthPool, countPool, weight, duration)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	default:
 		return "неизвестный тип тренировки"
